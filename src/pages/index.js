ee4b7c4d7d45073074d885db6ce6b36c87b9896d
plexiform <haaris.nyc@gmail.com>
2020-01-14 01:08:22 -0500

    family blog post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
+import Layout from "../components/layout.js"
+import { graphql, Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default ({ data }) =>
+  <Layout>
+    <h1><a href="https://www.beeminder.com/excursos/">beeminder</a></h1>
+    <h1><a href="https://dribbble.com/excursos">dribbble</a></h1>
+    <h1><a href="https://github.com/plexiform">github</a></h1>
+  </Layout>
+
+export const query = graphql`
+  {
+    allFile {
+      edges {
+        node {
+          base
+        }
+      }
+    }
+  }
+`
